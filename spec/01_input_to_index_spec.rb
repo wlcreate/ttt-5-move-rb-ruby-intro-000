2dd74fb670a9851a0449c37c6c9cdf62ad67d2c4
Waverley Leung <waverley.nyc@gmail.com>
2020-03-17 22:15:08 +0000

    Automatically backed up by Learn

--- spec/01_input_to_index_spec.rb
+++ spec/01_input_to_index_spec.rb
@@ -5,7 +5,9 @@ describe 'input_to_index' do
   it 'converts a user_input to an integer' do
     user_input = "1"
     converted_input = input_to_index(user_input)
-
+    def input_to_index
+      puts '5'.to_i
+    end    
     expect(converted_input).to be_a(Integer)
   end
 
@@ -13,13 +15,13 @@ describe 'input_to_index' do
     user_input = "6"
     converted_input = input_to_index(user_input)
 
-    expect('5').to_i
+    expect(converted_input).to be(5)
   end
 
   it 'returns -1 for strings without integers' do
     user_input = "invalid"
     converted_input = input_to_index(user_input)
-
+    
     expect(converted_input).to be(-1)
   end
 
